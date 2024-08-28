78cfd1fdbc79ff34fdb637aa5acda14e3dba59e3
Deepakm <deepakmautomation@gmail.com>
2024-08-28 22:50:26 +0530

    change driver from edge to chrome

--- src/test/java/testcases/LoginTest.java
+++ src/test/java/testcases/LoginTest.java
@@ -2,18 +2,19 @@ package testcases;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.Test;
 
 public class LoginTest {
+	
+	public WebDriver driver;
 
 
 	@Test
 	public void login_test() throws InterruptedException{
 
-		WebDriver driver;
-
-		driver = new EdgeDriver();
+		driver = new ChromeDriver();
 
 		driver.get("https://www.saucedemo.com/");
 
@@ -24,7 +25,11 @@ public class LoginTest {
 		driver.findElement(By.id("login-button")).click();
 
 		Thread.sleep(1000);
-
+		
+	}
+	
+	@AfterMethod
+	public void tearDown() {
 		driver.quit();
 	}
 
