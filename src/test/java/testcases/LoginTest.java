6359b23922f0d0f0f816c5d098fb0fff0d675f95
Deepakm <deepakmautomation@gmail.com>
2024-08-31 14:59:26 +0530

    add driver null check

--- src/test/java/testcases/LoginTest.java
+++ src/test/java/testcases/LoginTest.java
@@ -5,39 +5,48 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class LoginTest {
-	
-	public static WebDriver driver;
+
+	public  WebDriver driver;
+
+	@BeforeMethod
+	public void setup() {
+		System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
+
+		ChromeOptions ops = new ChromeOptions();
+
+		ops.addArguments("--headless");
+
+		driver = new ChromeDriver(ops);
+
+	}
 
 
 	@Test
 	public void login_test() throws InterruptedException{
-		
-		System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
-		
-		ChromeOptions ops = new ChromeOptions();
-		
-		ops.addArguments("--headless");
 
-		driver = new ChromeDriver(ops);
 
 		driver.get("https://www.saucedemo.com/");
 
 		driver.findElement(By.id("user-name")).sendKeys("standard_user");
 
 		driver.findElement(By.id("password")).sendKeys("secret_sauce");
-		
+
 		driver.findElement(By.id("login-button")).click();
 
 		Thread.sleep(1000);
-		
+
 	}
-	
+
 	@AfterMethod
 	public void tearDown() {
-		driver.quit();
+		if(driver !=null) {
+			driver.quit();
+		}
+		
 	}
 
 }
